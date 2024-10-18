fd9382c2724ea5863b2b163b2441755e2e6bc905
vintage254 <derricknjuguna414@gmail.com>
2024-10-18 03:41:58 +0300

    more deployment fixes

--- app/Http/Resources/UserCrudResource.php
+++ app/Http/Resources/UserCrudResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UsercrudResource extends JsonResource
+class UserCrudResource extends JsonResource
 {
     public static $wrap =false;
     /**
