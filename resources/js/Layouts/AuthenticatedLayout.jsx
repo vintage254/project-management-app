fcd0076357677a3a4d6e575d84df681a4786146f
vintage254 <derricknjuguna414@gmail.com>
2024-10-22 15:50:24 +0300

    fixing https production issues

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -36,6 +36,7 @@ export default function AuthenticatedLayout({ user, header, children }) {
                                 >
                                     All Tasks
                                 </NavLink>
+                                
                                 <NavLink
                                     href={route("user.index")}
                                     active={route().current("user.index")}
